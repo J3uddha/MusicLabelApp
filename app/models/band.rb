ca8561e9c4531c4a398dd2d102bc5b7475d360c9
j3uddha <jnelks@gmail.com>
2015-10-22 15:28:44 -0400

    Add Bands, Albums, User classes

---
+++ app/models/band.rb
@@ -0,0 +1,14 @@
+class Band < ActiveRecord::Base
+
+  validates :alias, presence: true
+
+  has_many :albums,
+  class_name: "Album",
+  foreign_key: :band_id,
+  primary_key: :id
+
+  has_many :tracks,
+  through: :albums,
+  source: :tracks
+
+end
